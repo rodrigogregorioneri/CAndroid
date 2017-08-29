e097346ef64e856b7fa386ca72acb8c46b9eabd6
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-29 10:16:10 -0300

    Adicionando comentarios explicativos

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
@@ -12,9 +12,16 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // Cria e popula Array de alunos "String"
     String [] alunos = {"Rodrigo", "Priscila","Ivone"};
+
+    //Cria obtém a ListView e faz uma Casting(Conversão) para ListView pois ela é um xml
     ListView lista_alunos = (ListView) findViewById(R.id.lista);
+
+    // Cria um Array Adapter e guarda na variavel adapter é necessario para converter um ARRAY em ListView
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+    // Associa a lista ao array adapter apartir disso já deve apresentar a lista na tela
     lista_alunos.setAdapter(adapter);
   }
 }
