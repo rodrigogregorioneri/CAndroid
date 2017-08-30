49b4a393eca37d951025c5788c6fa7ec9e52703b
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:05:12 -0300

    Instanciando o Intent responsavel pelas intenções do android, passando a activity atual como parametro e a que queremos ir

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rodrigoneri.cursoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
     aluno.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-
+          Intent intent = new Intent(MainActivity.this, FormActivity.class);
+        
       }
     });
 
