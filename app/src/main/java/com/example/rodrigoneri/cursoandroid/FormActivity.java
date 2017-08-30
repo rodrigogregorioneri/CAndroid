e69c7a1d16f5ba9b10abaf2babf18a1f7689d0ba
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:15:46 -0300

    Adicionando menu na ActionBar, iniciando sobrescrevendo o metodo onCreateOptionsMenu responsavel por adicionar o menu

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
@@ -2,6 +2,7 @@ package com.example.rodrigoneri.cursoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,6 +28,11 @@ public class FormActivity extends AppCompatActivity {
       }
     });
 
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    return super.onCreateOptionsMenu(menu);
   }
 }
 
