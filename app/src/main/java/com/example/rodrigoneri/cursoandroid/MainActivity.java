4350bb00377c3a1b3a798833e93b82df832d914a
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:01:43 -0300

    Criando o evento de clique com o callback

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rodrigoneri.cursoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
     // Instanciando o botão Aluno (Floating Button) feito nos commits anteriores
     Button aluno = (Button) findViewById(R.id.novo_aluno);
+
+    // Criando o evento de clique com o callback
+    aluno.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
 
 
   }
