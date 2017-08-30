863fa430c76b01c5b4627ffa618ba11eb30a04ff
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:45:20 -0300

    Sobrescrevendo metodo responsavel pelos eventos dos items de menu

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -36,6 +37,14 @@ public class FormActivity extends AppCompatActivity {
    MenuInflater inflate = getMenuInflater();
     inflate.inflate(R.menu.menu_formulario, menu);
     return super.onCreateOptionsMenu(menu);
+  }
+
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+
+    
+    return super.onOptionsItemSelected(item);
   }
 }
 
