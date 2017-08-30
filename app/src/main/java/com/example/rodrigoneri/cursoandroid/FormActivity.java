7ef482255eb3ef5490d102140f85f5a5e5c3fecf
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 18:03:01 -0300

    Adicionando evento de clique em FormActivity

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
@@ -2,6 +2,8 @@ package com.example.rodrigoneri.cursoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class FormActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_form);
+
+    Button botao_salvar = (Button) findViewById(R.id.formulario_salvar);
+
+    botao_salvar.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        
+      }
+    });
+
   }
 }
