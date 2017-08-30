934d47005f48f74f52a8c6d86d7447dcf9c907fc
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 18:10:02 -0300

    Comentando o codigo

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
@@ -13,11 +13,14 @@ public class FormActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_form);
 
+    //Instanciando o elemnto Button do Layout XMl e guardando na variavel botao_salvar do tipo Button
     Button botao_salvar = (Button) findViewById(R.id.formulario_salvar);
 
+    // Criando função de callback para executar o evento OnClick
     botao_salvar.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        // Imprimindo mensagem com Toast
         Toast.makeText(getBaseContext(), "teste",Toast.LENGTH_SHORT).show();
 
       }
