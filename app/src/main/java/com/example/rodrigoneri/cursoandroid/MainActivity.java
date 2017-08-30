9cd1a7a288cf8bcbcbec27e9698b5e27ac11fa98
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:06:37 -0300

    chamando startActivity que faz iniciar a activity é necessario passar o intent como parametro

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
           Intent intent = new Intent(MainActivity.this, FormActivity.class);
-        
+          startActivity(intent);
       }
     });
 
