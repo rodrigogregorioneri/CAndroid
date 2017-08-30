8616c0f09da8c55535ce493aba4a173e284ca86c
rodrigo.neri <rodrigogregorioneri@hotmail.com>
2017-08-30 19:33:47 -0300

    Adicionando o getMenuInflater e guardando em uma variavel inflate pra conseguir adicionar o menu, getInflate herda de AppCompatActivity

--- app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
+++ app/src/main/java/com/example/rodrigoneri/cursoandroid/FormActivity.java
@@ -3,6 +3,7 @@ package com.example.rodrigoneri.cursoandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,6 +33,7 @@ public class FormActivity extends AppCompatActivity {
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
+   MenuInflater inflate = getMenuInflater();
     return super.onCreateOptionsMenu(menu);
   }
 }
